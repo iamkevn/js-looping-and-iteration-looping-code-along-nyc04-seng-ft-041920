cad5230850f8d5e55478083dba164efeac7d7fe6
iamkevn <iamkevnrodriguez@gmail.com>
2020-06-21 13:22:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr, str) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${str} gift!`)
+    }
+    return newArr
+}
+
+function countDown (num) {
+    let i = num;
+     while (i <= num && i >= 0){
+         console.log(i--)
+     }
+ }
+
+ 
